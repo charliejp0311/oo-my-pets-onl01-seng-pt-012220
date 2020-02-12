774d841887141f3b737cccb6cd9cb5d75de6a7a6
Charles Pelton <charliejp0311@gmail.com>
2020-02-12 19:04:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
+require "pry"
 class Dog
   # code goes here
+  attr_accessor :owner
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    save 
+  end
+
+  def save 
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
 end

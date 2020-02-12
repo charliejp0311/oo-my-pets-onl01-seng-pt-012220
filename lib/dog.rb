3a8b7abba4e4eb2889dd7a73156cc702dbc4fb46
Charles Pelton <charliejp0311@gmail.com>
2020-02-12 19:05:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,15 @@
 require "pry"
 class Dog
   # code goes here
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   attr_reader :name
 
   @@all = []
 
-  def initialize(name, owner)
+  def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
+    @mood = mood
     save
   end
 

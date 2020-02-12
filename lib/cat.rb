b92fb1fb35eefeba69933415879e9aaf66b4d636
Charles Pelton <charliejp0311@gmail.com>
2020-02-12 19:00:36 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,9 +3,18 @@ class Cat
   # code goes here
   attr_accessor :owner, :mood
   attr_reader :name
+
+  @@all = []
+  
   def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
     @mood = mood
+    save 
   end
+
+  def save 
+    @@all << self 
+  end
+  
 end
